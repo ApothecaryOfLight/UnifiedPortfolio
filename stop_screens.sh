93dc9c7ae610f5f97a9594c2dcda78f00b41dfb2
ApothecaryOfLight <ChangerOfWays@gmail.com>
2022-03-27 13:56:49 +0000

    Added script to shutdown detached screen instances.

---
+++ stop_screens.sh
@@ -0,0 +1,7 @@
+screen -X -S Portfolio kill
+##screen -X -S DarkYears kill
+screen -X -S ketris_game kill
+screen -X -S ketris_chat kill
+screen -X -S ketris_content kill
+screen -X -S Flashcards kill
+screen -X -S Cineflix kill
