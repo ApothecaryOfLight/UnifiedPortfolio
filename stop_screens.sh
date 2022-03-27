d7bc4687ae56134c6d52fad3d276b7879b598b02
ApothecaryOfLight <ChangerOfWays@gmail.com>
2022-03-27 14:03:08 +0000

    Corrected screen names for Ketris in detached screen shutdown script.

--- stop_screens.sh
+++ stop_screens.sh
@@ -1,8 +1,8 @@
 screen -X -S Portfolio kill
 ##screen -X -S DarkYears kill
-screen -X -S ketris_game kill
+screen -X -S ketris_backend kill
 screen -X -S ketris_chat kill
-screen -X -S ketris_content kill
+screen -X -S ketris_frontend kill
 screen -X -S Flashcards kill
 screen -X -S Cineflix kill
 screen -X -S Oinker kill
