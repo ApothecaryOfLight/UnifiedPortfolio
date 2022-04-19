c35aaf8db45c061c4f405b776c2f3102fb837669
ApothecaryOfLight <ChangerOfWays@gmail.com>
2022-04-19 18:23:49 +0000

    Added Nginx configuration files.

--- install_scripts/install_nginx.sh
+++ install_scripts/install_nginx.sh
@@ -7,3 +7,8 @@ sudo ufw allow ssh
 sudo ufw enable
 
 sudo cp nginx_default_conf /etc/nginx/sites-available/default
+
+sudo cp nginx_configs/abesportfolio.com.config /etc/nginx/sites-available/default/abesportfolio.com.config
+sudo cp nginx_configs/cineflix.video.config /etc/nginx/sites-available/default/cineflix.video.config
+sudo cp nginx_configs/oinker.xyz.config /etc/nginx/sites-available/default/oinker.xyz.config
+sudo cp nginx_configs/triviacards.net.config /etc/nginx/sites-available/default/triviacards.net.config
